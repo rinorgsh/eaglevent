771e47e25c3ab685a0899e068aeb6aa9b635dac1
54718 <54718@etu.he2b.be>
2025-03-14 15:13:06 +0100

    route5

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware('guest')->group(function () {
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/home', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
