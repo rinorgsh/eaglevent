dc65416c36309a252c941c9a43ab0817a00a764a
54718 <54718@etu.he2b.be>
2025-03-14 12:36:19 +0100

    route3

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,10 @@ use Inertia\Inertia;
 
 
 
-Route::get('/', [DashboardController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
