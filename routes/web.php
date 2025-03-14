e1ad0f917b8320d7c0614ad1cbff17d8ebab416e
54718 <54718@etu.he2b.be>
2025-03-14 12:29:54 +0100

    route

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,6 @@ use Inertia\Inertia;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 
 Route::get('/', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
